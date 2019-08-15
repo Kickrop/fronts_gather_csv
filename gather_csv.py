148d86809dcac985314c360f836c09a1062e332e
martynov <kickrop@gmail.com>
2019-08-15 16:35:37 +0300

    tested open file

--- gather_csv.py
+++ gather_csv.py
@@ -4,14 +4,19 @@ path = 'H:\Fronts\jan2019\correction(aug19)'
 
 os.chdir(path)
 
-with open('1.csv', 'r') as f:
+"""with open('1.csv', 'r') as f:
     lines = f.readlines()[2:-2]
     with open('text.csv', 'a') as nf:
         for i in lines:
             nf.write(i)
-    print(lines)
+    print(lines)"""
 #        continue
    # else
     #    continue
 #pam = os.getcwd()
 
+for filename in os.listdir(path):
+    if filename.startswith('1.csv'):
+        with open('1.csv', 'r') as f:
+            lines = f.readlines()[2:-2]
+        print(lines)
