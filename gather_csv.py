6bb781c6b6b2d9c334aa516cef66d7ae95a9e788
martynov <kickrop@gmail.com>
2019-08-15 17:11:10 +0300

    test

--- gather_csv.py
+++ gather_csv.py
@@ -5,13 +5,11 @@ path = 'H:\Fronts\jan2019\correction(aug19)'
 
 os.chdir(path)
 
-#pam = os.getcwd()
-
 for filename in tqdm(os.listdir(path)):
     if filename.endswith('.csv'):
         with open(filename, 'r') as f:
             lines = f.readlines()[2:-2]
             with open('gathered.csv', 'a') as nf:
-                for i in lines:
+                for i in lines: 
                     nf.write(i)
         #print(lines)
