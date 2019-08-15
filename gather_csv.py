a9d18f616a6fd5f14355466267ca85214c415811
martynov <kickrop@gmail.com>
2019-08-15 17:07:01 +0300

    get the front id from filename

--- gather_csv.py
+++ gather_csv.py
@@ -6,11 +6,16 @@ os.chdir(path)
 
 #pam = os.getcwd()
 
+#for filename in os.listdir(path):
+#    if filename.startswith('1.csv'):
+#        with open('1.csv', 'r') as f:
+#            lines = f.readlines()[2:-2]
+#            with open('text.csv', 'a') as nf:
+#                for i in lines:
+#                    nf.write(i)
+#        #print(lines)
+
 for filename in os.listdir(path):
     if filename.startswith('1.csv'):
-        with open('1.csv', 'r') as f:
-            lines = f.readlines()[2:-2]
-            with open('text.csv', 'a') as nf:
-                for i in lines:
-                    nf.write(i)
-        #print(lines)
+        alt_fn = filename.split('.')
+        print(os.path.basename(alt_fn[0]))
