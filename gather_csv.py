ab06ee6eee26da4a5522461dc7483c06011a179a
martynov <kickrop@gmail.com>
2019-08-15 17:32:31 +0300

    got rid of unnecessary

--- gather_csv.py
+++ gather_csv.py
@@ -6,12 +6,12 @@ path = 'H:\Fronts\jan2019\correction(aug19)'
 os.chdir(path)
 
 for filename in tqdm(os.listdir(path)):
-    if filename.endswith('.csv'):
+    if filename.endswith('9999.csv'):
         with open(filename, 'r') as f:
             lines = f.readlines()[2:-2]
             with open('gathered.csv', 'a') as nf:
                 for i in lines:
                     id_front = os.path.basename(filename.split('.')[0])
-                    id_front_csv = f'"{id_front}",'
-                    nf.write(id_front_csv + i)
+                    #write id_front and papers of that front in one csv
+                    nf.write(f'"{id_front}",' + i)
 
